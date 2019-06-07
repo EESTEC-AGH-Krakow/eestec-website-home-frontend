0ea723e7641ebb18b36bcb35946846e7847759e8
Mateusz Piątkowski <mateusz.piatkowski@eestec.pl>
2019-06-07 22:38:45 +0200

    copy from hack-front-vue

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,53 @@
+const merge = require('babel-merge')
+const path = require('path')
+
 module.exports = {
-  productionSourceMap: false,
-  css: {
-    sourceMap: true,
-    loaderOptions: {
-      sass: {
-        data: `@import "@/assets/scss/partials/_variables.scss";
-        @import "@/assets/scss/partials.scss";
-        @import "@/assets/scss/main.scss";`
-      }
-    }
-  }
+	css: {
+		sourceMap: true,
+	},
+	
+	chainWebpack: config => {
+		config.module.rule('vue').
+			use('vue-loader').
+			loader('vue-loader').
+			tap(options => {
+				merge(options, {
+					transformToRequire: {
+						'img': 'src',
+						'image': 'xlink:href',
+						'b-img': 'src',
+						'b-img-lazy': ['src', 'blank-src'],
+						'b-card': 'img-src',
+						'b-card-img': 'img-src',
+						'b-carousel-slide': 'img-src',
+						'b-embed': 'src',
+					},
+				})
+			})
+		
+		const svgRule = config.module.rule('svg')
+		
+		svgRule.uses.clear()
+		
+		svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+	},
+	
+	publicPath: undefined,
+	outputDir: undefined,
+	assetsDir: 'assets',
+	runtimeCompiler: true,
+	productionSourceMap: undefined,
+	parallel: undefined,
+	
+	pluginOptions: {
+		'style-resources-loader': {
+			preProcessor: 'scss',
+			patterns: [
+				path.resolve(__dirname, './src/assets/sass/all.scss'),
+				path.resolve(__dirname, './node_modules/bootstrap/scss/bootstrap.scss'),
+        path.resolve(__dirname, './src/assets/sass/rwd.scss'),
+				path.resolve(__dirname, './src/assets/sass/animations.scss'),
+			],
+		},
+	},
 }
