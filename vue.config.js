4a8629a4365081c922088571ab21626b093bea5c
Mateusz Piątkowski <mateusz.piatkowski@eestec.pl>
2019-06-07 21:57:43 +0200

    changes in assets, vue config and more

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   productionSourceMap: false,
   css: {
-    sourceMap: true
+    sourceMap: true,
+    loaderOptions: {
+      sass: {
+        data: `@import "@/assets/scss/partials/_variables.scss";
+        @import "@/assets/scss/partials.scss";
+        @import "@/assets/scss/main.scss";`
+      }
+    }
   }
 }
