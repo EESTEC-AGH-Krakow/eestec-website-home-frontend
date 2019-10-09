dd5c20bd84c94332039824aeb5e01ee38cfc97e2
Mateusz Piątkowski <mateusz.piatkowski@eestec.pl>
2019-10-09 21:02:43 +0200

    shipping dist (cause mydevil sucks)

---
+++ dist/precache-manifest.4f3b25bcd643b4e07cdf828819f0d6a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+	{
+		'revision': '61abb825376359c7ac10',
+		'url': '/assets/css/app.b3f17c22.css',
+	},
+	{
+		'revision': '61abb825376359c7ac10',
+		'url': '/assets/js/app.6a7db39c.js',
+	},
+	{
+		'revision': '71fd9b12f5ba979113f1',
+		'url': '/assets/js/article.737e263c.js',
+	},
+	{
+		'revision': '18217edc86cae03928dd',
+		'url': '/assets/js/blog.e21b6fc5.js',
+	},
+	{
+		'revision': '8f6228048e1b41045ae5',
+		'url': '/assets/css/chunk-vendors.f9a53950.css',
+	},
+	{
+		'revision': '8f6228048e1b41045ae5',
+		'url': '/assets/js/chunk-vendors.dee0dec7.js',
+	},
+	{
+		'revision': 'd64d1873fa1b7bb1ff41165dd9b354b9',
+		'url': '/assets/img/logo_white.d64d1873.png',
+	},
+	{
+		'revision': '4003016c97fc8393ead4575afef54225',
+		'url': '/assets/img/background.4003016c.png',
+	},
+	{
+		'revision': '730e9ed94540484971a620a2d9f6d6e1',
+		'url': '/assets/fonts/Elegant.730e9ed9.otf',
+	},
+	{
+		'revision': 'dadfcd543826b8ddd3b33dcb008d19ee',
+		'url': '/index.html',
+	},
+	{
+		'revision': '4f0c3409a9d4844e8544311d759f5dbe',
+		'url': '/favicon.png',
+	},
+	{
+		'revision': 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+		'url': '/robots.txt',
+	},
+]
