e11143cc6ca3dcb9565af276f0949f323c1de84f
Mateusz Piątkowski <garadim12@gmail.com>
2020-03-21 13:40:52 +0100

    work on Projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,17 @@
 <template>
-	<b-row class="pb-4 pb-sm-5 text-black"  no-gutters>
+	<b-row class="pb-4 pb-sm-5 text-black" no-gutters>
 		<b-col cols="12">
 			<h1 class="text-center my-4 my-sm-5">Projekty</h1>
 		</b-col>
 		<b-col class="mx-auto" cols="11" lg="8" md="10">
 			<b-row>
-				<b-col :key="article.id" class="d-flex my-2" cols="12" sm="6" v-for="article in articles">
-<!--					<ArticleCard :article="article" />-->
+				<b-col cols="12">
+					<Carousel v-bind="carouselOptions">
+						<Slide :key="index" class="" v-for="(project, index) in projects">
+							<b-card :title="project.name" :img-src="project.image" :img-alt="project.name" img-top tag="article" style="" class="m-2">
+							</b-card>
+						</Slide>
+					</Carousel>
 				</b-col>
 			</b-row>
 		</b-col>
@@ -14,18 +19,59 @@
 </template>
 
 <script>
-    export default {
-        name: "Projects",
-        data() {
-            return {}
-        },
-        components: {},
-        methods: {},
-        computed: {},
-        watch: {}
-    }
+import { Carousel, Slide } from "vue-carousel";
+
+export default {
+	name: "Projects",
+	data() {
+		return {
+			carouselOptions: {
+				autoplay: true,
+				autoplayHoverPause: true,
+				perPage: 3
+			},
+			projects: [
+				{
+					name: "Hacknarök 1",
+					description: "Wspaniały maraton programistyczny, podczas którego Ty i Twoi dzielni kompani zawalczycie w bitwie o nowy, lepszy świat!",
+					image: require("@/assets/images/projects/hacknarok.png"),
+					link: "https://hacknarok.eestec.pl/"
+				},
+				{
+					name: "Hacknarök 2",
+					description: "Wspaniały maraton programistyczny, podczas którego Ty i Twoi dzielni kompani zawalczycie w bitwie o nowy, lepszy świat!",
+					image: require("@/assets/images/projects/hacknarok.png"),
+					link: "https://hacknarok.eestec.pl/"
+				},
+				{
+					name: "Hacknarök 3",
+					description: "Wspaniały maraton programistyczny, podczas którego Ty i Twoi dzielni kompani zawalczycie w bitwie o nowy, lepszy świat!",
+					image: require("@/assets/images/projects/hacknarok.png"),
+					link: "https://hacknarok.eestec.pl/"
+				},
+				{
+					name: "Hacknarök 4",
+					description: "Wspaniały maraton programistyczny, podczas którego Ty i Twoi dzielni kompani zawalczycie w bitwie o nowy, lepszy świat!",
+					image: require("@/assets/images/projects/hacknarok.png"),
+					link: "https://hacknarok.eestec.pl/"
+				},
+				{
+					name: "Hacknarök 5",
+					description: "Wspaniały maraton programistyczny, podczas którego Ty i Twoi dzielni kompani zawalczycie w bitwie o nowy, lepszy świat!",
+					image: require("@/assets/images/projects/hacknarok.png"),
+					link: "https://hacknarok.eestec.pl/"
+				}
+			]
+		};
+	},
+	components: {
+		Carousel,
+		Slide
+	},
+	methods: {},
+	computed: {},
+	watch: {}
+};
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
