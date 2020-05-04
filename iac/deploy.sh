1ac961b9a8c25e6e9b2431770bf23cc00cb1b061
Paweł Potaczała <pawel.potaczala@azimo.com>
2020-05-05 00:03:03 +0200

    clean repo

--- iac/deploy.sh
+++ iac/deploy.sh
@@ -3,6 +3,6 @@
 LOCATION=$1
 
 printf "sync files and remove unnecessary ...\n"
-rsync -Pavh -e 'ssh -i ./deploy_key' ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION" --delete-after
+rsync -Pavh -e 'ssh -i ./.deploy_key' ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION" --delete-after
 
 printf "\ndone, new version was released, check page in the browser!"
