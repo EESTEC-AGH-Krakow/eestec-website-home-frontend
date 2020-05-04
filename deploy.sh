61b5ef1a4771e009cb6d3c0dde019f21d6744f97
Paweł Potaczała <pawel.potaczala@azimo.com>
2020-05-04 18:53:59 +0200

    improve deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@
 
 LOCATION=$1
 
+openssl aes-256-cbc -K $encrypted_dfdcfd5172af_key -iv $encrypted_dfdcfd5172af_iv -in deploy_key.enc -out ./deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
+ssh -i ./deploy_key "$USERNAME"@"$SERVER_ADDRESS" pwd
 scp -r ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION"
