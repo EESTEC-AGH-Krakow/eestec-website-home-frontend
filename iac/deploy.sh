b3c95c724dde2867359b6cfb8105ae7613471d9f
Paweł Potaczała <pawel.potaczala@azimo.com>
2020-05-04 22:39:29 +0200

    fix rsync

--- iac/deploy.sh
+++ iac/deploy.sh
@@ -3,6 +3,6 @@
 LOCATION=$1
 
 echo "sync files and remove unnecessary ..."
-rsync -Pavh -e \"ssh -i ~/deploy_key\" ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION" --delete-after
+rsync -Pavh -e 'ssh -i ./deploy_key' ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION" --delete-after
 
-echo "done, new version was released, check page in the browser!"
+echo "\ndone, new version was released, check page in the browser!"
