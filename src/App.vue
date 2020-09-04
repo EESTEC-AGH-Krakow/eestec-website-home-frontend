71c581d03468ac8deb5c4fed15170b563b0f381c
Mateusz Piątkowski <garadim12@gmail.com>
2020-09-04 22:55:41 +0200

    create articles get method action

--- src/App.vue
+++ src/App.vue
@@ -36,9 +36,10 @@ export default {
 	},
 	mounted() {
 		this.changeLoading();
+		this.getPosts();
 	},
 	methods: {
-		...mapActions(['changeLoading']),
+		...mapActions(['changeLoading', 'getPosts']),
 	},
 	computed: {
 		largerDevice() {
