6a0966011b994654bd8518340634ad6891250b9c
Mateusz Piątkowski <garadim12@gmail.com>
2020-09-04 22:55:41 +0200

    create articles get method action

--- src/store.js
+++ src/store.js
@@ -1,15 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		isLoading: true
+		isLoading: true,
+		posts: []
 	},
 	mutations: {
 		loading(state, payload) {
 			state.isLoading = payload;
+		},
+		loadPosts(state, payload) {
+			state.posts = payload;
 		}
 	},
 	actions: {
@@ -17,6 +22,14 @@ export default new Vuex.Store({
 			setTimeout(() => {
 				commit("loading", false);
 			}, 3000);
+		},
+		getPosts({ commit }) {
+			axios({
+				method: "get",
+				url: `${process.env.VUE_APP_BACKEND_API_HOST_URL}/posts&categories=25`
+			})
+				.then(res => res.data.map(({ title, date, content }) => ({ title, date, content })))
+				.then(posts => commit("loadPosts", posts));
 		}
 	}
 });
