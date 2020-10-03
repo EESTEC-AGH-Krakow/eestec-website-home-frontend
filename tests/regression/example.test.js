1daa5c5c18a5373c2f282b2ae7bbfa29ffbce385
Mateusz Piątkowski <garadim12@gmail.com>
2020-10-03 23:24:24 +0200

    add AVA as test runner

---
+++ tests/regression/example.test.js
@@ -0,0 +1,8 @@
+// TODO: get familiarized with and implement below solutions
+// https://vue-test-utils.vuejs.org/
+// https://github.com/avajs/ava/blob/master/docs/recipes/vue.md
+import test from 'ava';
+
+test.skip('renders props.msg when passed', t => {
+	t.truthy(true);
+});
